e1ea0d7ba19ffe08e9eba39e5a76541ee44e694f
Flavio Avdulla <a.flavio4366@gmail.com>
2025-02-18 15:27:04 +0100

    crud_operations

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import CreateUser from "./components/CreateUser/CreateUser";
+import UpdateUser from "./components/UpdateUser/UpdateUser";
+import DeleteUser from "./components/DeleteUser/DeleteUser";
+import ReadUser from "./components/ReadUser/ReadUser";
+import HomePage from "./components/HomePage/HomePage";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage /> } />
+          <Route path="/create-user" element={<CreateUser />} />
+          <Route path="/read-user" element={<ReadUser />} />
+          <Route path="/update-user" element={<UpdateUser />} />
+          <Route path="/delete-user" element={<DeleteUser />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
